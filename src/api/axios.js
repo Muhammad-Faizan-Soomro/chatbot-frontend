d1b3bebb4580e3485dbea7c8ffe04bbe15eee95a
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2025-06-14 03:41:39 +0500

    update url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://ec2-13-201-73-171.ap-south-1.compute.amazonaws.com:8000",
   withCredentials: false, // replace with your FastAPI base URL
 });
 
